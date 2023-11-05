6e2e817af8c339bc92c6d5ed5658dce66b7ff0c0
liderun <ldr503@outlook.com>
2023-11-05 22:06:03 +0800

    项目初始化

---
+++ src/main/java/com/example/springboot3vue3/SpringBoot3Vue3Application.java
@@ -0,0 +1,13 @@
+package com.example.springboot3vue3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBoot3Vue3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBoot3Vue3Application.class, args);
+    }
+
+}
